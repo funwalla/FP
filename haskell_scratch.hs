34ef2655bed4a9a6f058a28afa782bff8357274a
funwalla <johnpaulwinchester@gmail.com>
2014-05-06 17:19:33 -0700

    README added to repository. Comments added to haskell_scratch.hs while learning git.

--- haskell_scratch.hs
+++ haskell_scratch.hs
@@ -6,3 +6,6 @@ fl f acc (x:xs) =  fl f (f acc x) xs
 fr              :: (a -> b -> b) -> b -> [a] -> b
 fr f acc []     =  acc
 fr f acc (x:xs) =  f x (fr f acc xs)
+
+-- comment added while learning git.
+-- one more change.
